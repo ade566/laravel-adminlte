b8ed2cac6d41b0a8a7803c38536e144fb42c8b7a
Muhamad Ade Rohayat <muhamadaderohayat122@gmail.com>
2023-08-22 20:57:38 +0700

    dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+  <section class="content p-3">
+    <h5>Halo {{auth()->user()->name}}...</h5>
+  </section>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,8 +23,8 @@ Route::post('login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
 
-    Route::get('/dashboard', function () {
-        return 'dashboard';
-    })->name('home');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
 });
